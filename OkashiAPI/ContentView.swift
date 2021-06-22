74a49db3ad825939f762657534b7957e3a8ded01
Miyakawa Takuya <omyakawa@gmail.com>
2021-06-22 11:47:06 +0900

    Initial Commit

---
+++ OkashiAPI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  OkashiAPI
+//
+//  Created by 宮川卓也 on 2021/06/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ OkashiAPI/OkashiAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  OkashiAPIApp.swift
+//  OkashiAPI
+//
+//  Created by 宮川卓也 on 2021/06/22.
+//
+
+import SwiftUI
+
+@main
+struct OkashiAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
